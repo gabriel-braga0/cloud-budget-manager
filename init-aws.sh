59449dc2cb35867032c8075211c221b64907cd08
gabriel <bragagabriel843@gmail.com>
2026-04-28 15:31:56 -0300

    lambda connection

--- init-aws.sh
+++ init-aws.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-echo "🚀 Iniciando a criação da infraestrutura no LocalStack..."
+echo "🚀 Iniciando o Deploy da Infraestrutura com AWS CloudFormation..."
 
-awslocal dynamodb create-table \
-    --region us-east-1 \
-    --table-name CloudBudgetTable \
-    --attribute-definitions AttributeName=PK,AttributeType=S AttributeName=SK,AttributeType=S AttributeName=GSI1PK,AttributeType=S AttributeName=GSI1SK,AttributeType=S \
-    --key-schema AttributeName=PK,KeyType=HASH AttributeName=SK,KeyType=RANGE \
-    --global-secondary-indexes '[{"IndexName": "GSI1","KeySchema":[{"AttributeName":"GSI1PK","KeyType":"HASH"},{"AttributeName":"GSI1SK","KeyType":"RANGE"}],"Projection":{"ProjectionType":"ALL"}}]' \
-    --billing-mode PAY_PER_REQUEST
+awslocal cloudformation deploy \
+    --template-file /tmp/template.yaml \
+    --stack-name budget-stack \
+    --region us-east-1
 
-awslocal sns create-topic --name expense-alerts-topic
-
-awslocal sqs create-queue --queue-name expense-alerts-queue
-
-awslocal sns subscribe \
-    --topic-arn arn:aws:sns:us-east-1:000000000000:expense-alerts-topic \
-    --protocol sqs \
-    --notification-endpoint arn:aws:sqs:us-east-1:000000000000:expense-alerts-queue
-
-echo "✅ Infraestrutura pronta e vinculada!"
+echo "✅ Deploy via IaC finalizado com sucesso!"
